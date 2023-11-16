193a4c19c8c5c72dc9996fc2bccdc6e08ce5f9e2
PeizhongQiu <15608652022@163.com>
2023-11-16 06:57:24 +0000

    exercise 3

--- apps/loader/src/main.rs
+++ apps/loader/src/main.rs
@@ -7,13 +7,14 @@ use alloc::vec::Vec;
 #[cfg(feature = "axstd")]
 use axstd::println;
 const PLASH_START: usize = 0x22000000;
+const RUN_START: usize = 0xffff_ffc0_8010_0000;
 
 
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
     let num_app_ptr = PLASH_START as usize as *const usize;
     let num_app = unsafe { num_app_ptr.read_volatile() };
-    let mut app_start: Vec<usize>  =  unsafe { core::slice::from_raw_parts(num_app_ptr.add(1), num_app * 2).to_vec()};
+    let app_start: Vec<usize>  =  unsafe { core::slice::from_raw_parts(num_app_ptr.add(1), num_app * 2).to_vec()};
         
     println!("Load payload ...");
     for i in 0..num_app {
@@ -21,7 +22,24 @@ fn main() {
         let size = app_start[i*2+1];
         println!("{} {}", start as usize, size);
         let code = unsafe { core::slice::from_raw_parts(start, size)};
-        println!("content: {:?}: ", code);
+        println!("load code {:?}; address [{:?}]", code, code.as_ptr());
+        // app running aspace
+        // SBI(0x80000000) -> App <- Kernel(0x80200000)
+        // 0xffff_ffc0_0000_0000
+        let new_addr = RUN_START as usize + app_start[i*2] - app_start[0];
+        let run_code = unsafe {
+            core::slice::from_raw_parts_mut(new_addr as *mut u8, size)
+        };
+        run_code.copy_from_slice(code);
+        println!("run code {:?}; address [{:?}]", run_code, run_code.as_ptr());
+        println!("Execute app ...");
+
+        // execute app
+        unsafe { core::arch::asm!("
+            mv      t2, {run_start}
+            jalr    t2",
+            run_start = in(reg) new_addr,
+        )}
     }
 
     println!("Load payload ok!");
